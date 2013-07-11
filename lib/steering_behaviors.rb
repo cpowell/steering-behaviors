761314e0d1e4f0760bc2ecd0eb25e3c6ad0790f3
Chris Powell <cpowell@prylis.com>
2013-07-11 14:58:15 -0700

    Underscores.

--- lib/steering_behaviors.rb
+++ lib/steering_behaviors.rb
@@ -1,11 +1,11 @@
 class SteeringBehaviors
 end
 
-require 'steering-behaviors/vector.rb'
-require 'steering-behaviors/steering.rb'
-require 'steering-behaviors/seek.rb'
-require 'steering-behaviors/flee.rb'
-require 'steering-behaviors/arrive.rb'
-require 'steering-behaviors/pursue.rb'
-require 'steering-behaviors/evade.rb'
-require 'steering-behaviors/wander.rb'
+require 'steering_behaviors/vector.rb'
+require 'steering_behaviors/steering.rb'
+require 'steering_behaviors/seek.rb'
+require 'steering_behaviors/flee.rb'
+require 'steering_behaviors/arrive.rb'
+require 'steering_behaviors/pursue.rb'
+require 'steering_behaviors/evade.rb'
+require 'steering_behaviors/wander.rb'
