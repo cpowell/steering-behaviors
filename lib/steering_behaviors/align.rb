982bdc37875cfeebbb56ffaf17b0b8854dfbe2d4
Chris Powell <cpowell@prylis.com>
2013-07-22 13:49:31 -0700

    This version of align seems to be good and tidy.

--- lib/steering_behaviors/align.rb
+++ lib/steering_behaviors/align.rb
@@ -5,21 +5,18 @@ class SteeringBehaviors::Align
   # * *Args*    :
   #   - +hunter_kinematic+ -> pursuing kinematic
   #   - +quarry_kinematic+ -> kinematic of the target
+  #   - +alignment_threshold+ -> consider aligned if the difference < this val
   # * *Returns* :
   #   -
   # * *Raises* :
   #   - ++ ->
   #
-  def self.steer(hunter_kinematic, quarry_kinematic, alignment_threshold, multiplier=8 )
+  def self.steer(hunter_kinematic, quarry_kinematic, alignment_threshold)
     difference = ( ( quarry_kinematic.heading_vec.radians - hunter_kinematic.heading_vec.radians + 3*Math::PI ) % (2*Math::PI) ) - Math::PI
 
     return SteeringBehaviors::Vector.new(0,0) if difference.abs <= alignment_threshold
 
-    if difference > 0
-      target_local = SteeringBehaviors::Vector.new(multiplier, 0)
-    else
-      target_local = SteeringBehaviors::Vector.new(-multiplier, 0)
-    end
+    target_local = SteeringBehaviors::Vector.new(difference * hunter_kinematic.speed, 0)
 
     target_local.rotate!(hunter_kinematic.heading_vec.radians)
   end
