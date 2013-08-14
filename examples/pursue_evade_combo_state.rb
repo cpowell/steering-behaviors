84bae2610495df61ac026ca20e99c69bd4cb26f1
Chris Powell <cpowell@prylis.com>
2013-08-13 17:05:39 -0700

    Additional cosmetics for pursue/evade demo.

--- examples/pursue_evade_combo_state.rb
+++ examples/pursue_evade_combo_state.rb
@@ -34,16 +34,18 @@ class PursueEvadeComboState < BasicGameState
 
     randomize_things
 
-    @character_img = Circle.new(@character.position_vec.x, @character.position_vec.y, 5)
-
-    @tgt_img = Circle.new(@enemy.position_vec.x, @enemy.position_vec.y, 5)
+    @character_img = Circle.new(@character.position_vec.x, @character.position_vec.y, 6)
+    @enemy_img = Circle.new(@enemy.position_vec.x, @enemy.position_vec.y, 6)
 
     # Visual artifacts to illustrate what's going on...
-    @heading_line = Line.new(@character.position_vec.x, @character.position_vec.y, (@character.position_vec.x + @character.heading_vec.x * VISUAL_SCALE), (@character.position_vec.y + @character.heading_vec.y * VISUAL_SCALE))
-    @steering_force_line  = Line.new(@character.position_vec.x, @character.position_vec.y, (@character.position_vec.x + @character.heading_vec.x * VISUAL_SCALE), (@character.position_vec.y + @character.heading_vec.y * VISUAL_SCALE))
+    @char_heading_line = Line.new(@character.position_vec.x, @character.position_vec.y, (@character.position_vec.x + @character.heading_vec.x * VISUAL_SCALE), (@character.position_vec.y + @character.heading_vec.y * VISUAL_SCALE))
+    @enemy_heading_line = Line.new(@enemy.position_vec.x, @enemy.position_vec.y, (@enemy.position_vec.x + @enemy.heading_vec.x * VISUAL_SCALE), (@enemy.position_vec.y + @enemy.heading_vec.y * VISUAL_SCALE))
 
-    @char_future_pos_img = Circle.new(0, 0, 4)
-    @enemy_future_pos_img = Circle.new(0, 0, 4)
+    @char_steering_force_line  = Line.new(@character.position_vec.x, @character.position_vec.y, (@character.position_vec.x + @character.heading_vec.x * VISUAL_SCALE), (@character.position_vec.y + @character.heading_vec.y * VISUAL_SCALE))
+    @enemy_steering_force_line  = Line.new(@enemy.position_vec.x, @enemy.position_vec.y, (@enemy.position_vec.x + @enemy.heading_vec.x * VISUAL_SCALE), (@enemy.position_vec.y + @enemy.heading_vec.y * VISUAL_SCALE))
+
+    @char_future_pos_img = Circle.new(0, 0, 3)
+    @enemy_future_pos_img = Circle.new(0, 0, 3)
   end
 
   # The update method is called during the game to update the logic in our
@@ -83,19 +85,25 @@ class PursueEvadeComboState < BasicGameState
     @character_img.setCenterX @character.position_vec.x
     @character_img.setCenterY @character.position_vec.y
 
-    @tgt_img.setCenterX @enemy.position_vec.x
-    @tgt_img.setCenterY @enemy.position_vec.y
+    @enemy_img.setCenterX @enemy.position_vec.x
+    @enemy_img.setCenterY @enemy.position_vec.y
 
     @char_future_pos_img.setCenterX predicted_char_position.x
     @char_future_pos_img.setCenterY predicted_char_position.y
     @enemy_future_pos_img.setCenterX predicted_enemy_position.x
     @enemy_future_pos_img.setCenterY predicted_enemy_position.y
 
-    @heading_line.set @character.position_vec.x, @character.position_vec.y, (@character.position_vec.x + @character.heading_vec.x * VISUAL_SCALE), (@character.position_vec.y + @character.heading_vec.y * VISUAL_SCALE)
+    @char_heading_line.set @character.position_vec.x, @character.position_vec.y, (@character.position_vec.x + @character.heading_vec.x * VISUAL_SCALE), (@character.position_vec.y + @character.heading_vec.y * VISUAL_SCALE)
 
-    @steering_force_line.set @character.position_vec.x, @character.position_vec.y,
+    @enemy_heading_line.set @enemy.position_vec.x, @enemy.position_vec.y, (@enemy.position_vec.x + @enemy.heading_vec.x * VISUAL_SCALE), (@enemy.position_vec.y + @enemy.heading_vec.y * VISUAL_SCALE)
+
+    @char_steering_force_line.set @character.position_vec.x, @character.position_vec.y,
       (char_steering_force.x + @character.position_vec.x),
       (char_steering_force.y + @character.position_vec.y)
+
+    @enemy_steering_force_line.set @enemy.position_vec.x, @enemy.position_vec.y,
+      (enemy_steering_force.x + @enemy.position_vec.x),
+      (enemy_steering_force.y + @enemy.position_vec.y)
 
     # If the enemy catches the char, start over
     if (@enemy.position_vec.x - @character.position_vec.x).abs < 10 && (@enemy.position_vec.y - @character.position_vec.y).abs < 10
@@ -121,20 +129,22 @@ class PursueEvadeComboState < BasicGameState
     g.draw_string(data, @enemy.position_vec.x+10, @enemy.position_vec.y+10)
 
     g.setColor(Color.green)
-    g.draw(@character_img)
+    g.draw @character_img
+    g.draw @char_future_pos_img
 
     g.setColor(Color.red)
-    g.draw @heading_line
+    g.draw @char_heading_line
+    g.draw @enemy_heading_line
 
     g.setColor(Color.cyan)
-    g.draw @steering_force_line
+    g.draw @char_steering_force_line
+    g.draw @enemy_steering_force_line
 
     g.setColor(Color.yellow)
-    g.draw @tgt_img
+    g.draw @enemy_img
     g.draw @enemy_future_pos_img
 
     g.setColor(Color.cyan)
-    g.draw @char_future_pos_img
   end
 
   # Notification that a key was released
@@ -160,6 +170,6 @@ class PursueEvadeComboState < BasicGameState
   # Place the quarry somewhere random...
   def randomize_things
     @character = Bug.new(MAX_X/2, MAX_Y/2, rand(360), rand(5..50), rand(0.2..1.3), Math::PI/2, 5, rand(30..60))
-    @enemy = Bug.new(rand(MAX_X/2..MAX_X), rand(MAX_Y/2..MAX_Y), rand(360), rand(5..75), rand(0.2..1.3), Math::PI/4, 5, rand(40..150))
+    @enemy = Bug.new(rand(0..MAX_X), rand(0..MAX_Y), rand(360), rand(5..75), rand(0.2..1.3), Math::PI/4, 5, rand(40..150))
   end
 end
